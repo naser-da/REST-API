08e8368da5296279803122591d1607d97f0db880
Naser Dakhel <naser.dakhel@raizer.tech>
2023-05-12 23:19:59 +0300

    Filter data

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -2,21 +2,32 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Filters\V1\CustomersFilter;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 use App\Models\Customer;
 use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return new CustomerCollection(Customer::paginate());
+        $filter = new CustomersFilter();
+        $queryItems = $filter->transform($request);
+
+        if(count($queryItems) == 0)
+        {
+            return new CustomerCollection(Customer::paginate());
+        } else {
+            $customers = Customer::where($queryItems)->paginate();
+            return new CustomerCollection($customers->appends($request->query()));
+        }
     }
 
     /**
